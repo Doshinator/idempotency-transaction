23a607479b4a07c8eacd70dc09ffa61a285ad8fa
Rushabh Doshi <doshi@Rushabhs-MacBook-Pro.local>
2025-12-09 17:51:41 -0500

    backend wired up and localhost 8080 up & running

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+use serde::{Deserialize, Serialize};
+use sqlx::{PgPool, prelude::FromRow};
+use uuid::Uuid;
+
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+struct Transaction {
+    id: Uuid,
+    user_session_id: Uuid,
+    name: String,
+    amount: f64,
+    email: String,
+    description: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateTransactionRequest {
+    name: String,
+    amount: f64,
+    email: String,
+    description: String,
+}
+
+struct AppState {
+    db: PgPool,
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let database_url = "posgres://postgres:postgres@localhost/transactions";
+    let pool = PgPool::connect(database_url)
+        .await
+        .expect("Failed connecting to db");
+    let app_state = web::Data::new(AppState { db: pool});
+
+    println!("Starting Idempotency Transaction checker API on localhost port 8080");
+
+    HttpServer::new(move || {
+        App::new()
+        .app_data(app_state.clone())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
